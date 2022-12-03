79f39554aace8b2ed2830f4f02edeb07452bfe03
NoemiHuarino-utec <91573449+NoemiHuarino-utec@users.noreply.github.com>
2022-12-02 23:32:52 -0500

    update_inicio

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,17 @@
 <template>
-  <v-app class = "contenedor"> <!-- esto es como un div -->
+    <v-app class = "contenedor"> <!-- esto es como un div -->
       <!-- Encabezado -->
-      <v-app-bar  color = "black" absolute = true class = "barraNav"> 
 
-        <v-toolbar-title> CyM Login</v-toolbar-title>  <!-- titulo -->
+      <v-app-bar  color = "black" absolute = true class = "barraNav"> 
+         <!-- logo - imagen -->
+        <img
+          alt="Vue logo"
+          class="logo"
+          src="@/assets/logoCYM.jpg"
+          width="70"
+          height="70"
+         />
+        <v-toolbar-title> Chino y Menacho</v-toolbar-title>  <!-- titulo -->
         <v-spacer></v-spacer> <!-- ezquina derecha -->
 
         <!-- Botones de encabezado -->
@@ -13,28 +21,26 @@
         <v-btn text rounded> Regístrate</v-btn>
 
       </v-app-bar>
- 
-
 
 
 
       <!-- Home section start-->
+
       <section class="home" id="home">
         <div class="content">
-          <h3 align="center">CyM</h3>
-
-          <span align="center"> ¡Fiestas juveníles!</span>
-
-          <p align="center"> Lorem iaspoesinfeipsnefsnefisnfsef evwfvse ve gev
-            veve vevs bre ge  rgeerrerrbrer erbrre.
-          </p>
-
-          <a href="#" class="btn">shop now</a>
+          <h3 align="center">Chino y Menacho</h3>
+          <span align="center"> Emprendimiento que organiza fiestas juveniles</span>
+          <!--<a href="#" class="btn">shop now</a>-->
 
         </div>
       </section>
 
       <!-- Home section end-->
+
+
+<section class="full-width formated-section">
+  
+</section>
 
 
       <!-- inferior-->
@@ -66,8 +72,7 @@
   </v-app>  <!-- cerrando div class-->
 </template>
 
-
-<script> // JavaScript
+<script> // JavaScript para la parte "CONTACTO"
 export default{
   data() 
   {
@@ -86,6 +91,8 @@ export default{
 
 
 <style scoped>
+
+/*
 .barraNav{
   z-index:0
 }
@@ -98,6 +105,64 @@ export default{
 .section{
   padding:2rem 9%;
 }
+
+/* col-sm */
+@media (min-width: 768px) and (max-width: 991px) {
+    
+  }
+  
+  /* col-md */
+  @media (min-width: 992px) and (max-width: 1199px) {
+      
+  }
+  
+  /* col-lg */
+  @media (min-width: 1200px) {
+      
+  }
+
+
+.home{
+  position: relative;
+	width: 100%;
+	height: calc(100vh - 50px);
+	background-color: #F5F5F5;
+	background-size: cover;
+	background-position: center;
+	transition: all .1s ease-out;
+	background-image: url('assets/fiesta2.jpeg');
+	animation: banner 28s infinite linear;
+}
+
+.home .content{
+  margin: 0;
+	padding: 0;
+	width: 100%;
+	height: 100%;
+	color: #FFF;
+	background-color: rgba(255, 252, 247, 0.148);
+	display: flex;
+	flex-direction: column;
+	justify-content: center;
+	align-items: center;
+
+}
+
+.home .content h3{
+    font-size: 6rem;
+    color: rgb(0, 0, 0);
+}
+
+.home .content span{
+  text-decoration: none;
+	color: rgb(255, 255, 255);
+	padding: 9px 20px;
+	text-transform: uppercase;
+  transition: all .3s ease-in-out;
+  border: 1px solid rgb(255, 255, 255);
+}
+
+/*
 .home{
     display: flex;
     align-items: center;
@@ -130,7 +195,7 @@ export default{
     padding:1rem 0;
     line-height: 1.5;
 }
-
+*/
 .home .content p{
     font-size: 1.5rem;
     color:rgb(255, 255, 255);
@@ -138,3 +203,4 @@ export default{
     line-height: 1.5;
 }
 </style>
+
